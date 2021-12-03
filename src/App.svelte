49b2111da373c420b32d66fca59d9d34767d1c1b
Robin Nash <84207317+RobinNash@users.noreply.github.com>
2021-12-03 02:25:09 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,28 @@
 
 <script>
-//https://stackoverflow.com/questions/56483209/import-css-in-node-modules-to-svelte
-//https://svelte.dev/blog/the-easiest-way-to-get-started  //install svelte
-//https://www.codegrepper.com/code-examples/javascript/get+mouse+position+javascript  cursor function i stole
     const w = 70;
     const cm = 5; //cursor margin, for cursor select
-    const cx = 0; const cy = 0; // center image under cursor
-	let images = [
+    const cx = -30; const cy = -300; // center image under cursor
+    const ch = 750; const cw = 1490; //canvas height canvas width
+	const images = [
 		{ id: 1, text: `cat`, name: `cat.png` , width: w},
 		{ id: 2, text: `dog`, name: `dog.png` , width: w},
 		{ id: 3, text: `dino`, name: `dino.png`, width: w}
 	];
+    
+    const colors = ['magenta','red','orange','yellow','rgb(0,255,0)','green','cyan','blue','rgb(120,0,255)','purple','black','white'];
+
+    let selectedColor = { i: 9, color: colors[9]};
 
 	let selected = images[0];
     
     let top = 1000;
+    let bottom;
+    let right;
+    let left;
     let start = false;
     
-    let colors = false;
+    let checkcolors = false;
 
     
     function getCoords(event){
@@ -39,8 +44,8 @@
         let coors = getCoords(event);
         let x = coors[0];
         let y = coors[1];
-        if (y > top && start)
-            document.getElementById("canvas").innerHTML = document.getElementById("canvas").innerHTML + `<img src="${selected.name}" style="position:absolute; left: ${x}px; top: ${y}px; width: ${selected.width}px">`;
+        if (start && y > top && y < bottom && x < right && x > left)
+            document.getElementById("canvas").innerHTML = document.getElementById("canvas").innerHTML + `<img id="drawn" src="${selected.name}" style="position:absolute; left: ${x}px; top: ${y}px; width: ${selected.width}px">`;
     }
 
     function selectCursor(image){
@@ -57,8 +62,25 @@
             document.getElementById(id).style.display='none';
     }
     
+    function changeCanvasColor(){
+        //selectedColor = { i: ((selectedColor.i+1) % colors.length)
+        selectedColor.i++;
+        selectedColor.i = selectedColor.i % colors.length;
+        selectedColor.color = colors[selectedColor.i];
+        document.getElementById('canvas').style.backgroundColor = selectedColor.color;
+    }
+    
+    function clearScreen() {
+        location.reload(); 
+        //document.getElementById("drawn").style.display='none';
+    }
+    
     function handleStart(event) {
-        top = getCoords(event)[1];
+        let coors = getCoords(event);
+        left = coors[0];
+        top = coors[1] + 25;
+        bottom = top + ch - w;
+        right = left + cw - w - 30;
         start = true;
         document.getElementById('start').style.display='none';
     }
@@ -68,40 +90,41 @@
 	}
     
     function tester (msg) {
+    //on:click={() => tester("hi")}
         console.log(msg);
     }
     console.log("hello");
     
     $: {console.log(`the selected image is ${selected.text}`); }
+    
+    //$: {document.getElementById('canvas').style.backgroundColor = ${selectedColor.color};}
+    //onclick="this.style.backgroundColor=selectedColor.color;"
 </script>
 
-<h2>This is a paragraph.</h2>
+<h1>WELCOME</h1>
+<h4>Click in the canvas to change its color!</h4>
+<p>App by Robin Nash</p>
+    
 
-<style>
-	h2 { background-color: #ffaaff }
-</style>
 
 <button value="Select Cursor" on:click={() => toggleMenu('cursor-select')}>Select Cursor</button>
-<button on:click={() => tester("hi")}>Clear Screen</button>
+<button on:click={clearScreen}>Clear Screen</button>
 
-{#if colors}
+{#if checkcolors}
 <input type=checkbox style="background-color:#0000ff;background:#0000ff;color:#0000ff">
 <input type=checkbox style="background-color:#ff00ff;background:#00ffff;color:#ff0000">
 {/if}
 
-<div id="cursor-select" style="display:none; position:relative; background-color: #505050; top:0px;left:0px;width:{w * 3 + 5 * 3}px">
+<div id="cursor-select" style="display:none; position:relative; top:0px;left:0px;width:{ (w + cm) * images.length + cm}px">
     {#each images as image}
-        <img id="cursor" src={image.name} on:click={() => selectCursor(image)} style="display:inline; width:{image.width}px; position:inline">
+        <img id="cursor" src={image.name} on:click={() => selectCursor(image)} style="display:inline; width:{image.width}px; position:inline; margin-left:{cm}px; margin-top:{cm}px">
     {/each}
 </div>
 
-<div style="position:absolute;
-width:1000px;
-height:1000px;
-border:5px groove black;" on:mousemove={drawImage} id="canvas"> 
-<button id="start" on:click={handleStart} style="position:absolute; margin:10px;width:50px;height:50px">Start</button>
+<div style="position:absolute; width:{cw}px; height:{ch}px; margin-bottom:5px;
+background-color:#330099;
+border:5px solid black;" on:mousemove={drawImage} on:click={changeCanvasColor} id="canvas"> 
+<button id="start" on:click={handleStart} style="position:absolute; margin:10px;width:50px;height:50px;background-color:#ff22cc">Start</button>
 </div>
-    
-    
-    
+
   
